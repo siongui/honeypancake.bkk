e2e9d41d45986a0a2407ea941ff9c816c104ce7e
siongui <siongui@gmail.com>
2020-09-15 11:31:35 +0800

    try pwa again

--- content/extra/pwabuilder-sw.js
+++ content/extra/pwabuilder-sw.js
@@ -1,18 +1,12 @@
-// This is the "Offline page" service worker
+// This is the "Offline copy of assets" service worker
+
+
+
+const CACHE = "pwabuilder-offline";
 
 
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
-
-
-
-const CACHE = "pwabuilder-page";
-
-
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-
-const offlineFallbackPage = "ToDo-replace-this-name.html";
 
 
 
@@ -28,68 +22,14 @@ self.addEventListener("message", (event) => {
 
 
 
-self.addEventListener('install', async (event) => {
+workbox.routing.registerRoute(
 
-  event.waitUntil(
+  new RegExp('/*'),
 
-    caches.open(CACHE)
+  new workbox.strategies.StaleWhileRevalidate({
 
-      .then((cache) => cache.add(offlineFallbackPage))
+    cacheName: CACHE
 
-  );
+  })
 
-});
-
-
-
-if (workbox.navigationPreload.isSupported()) {
-
-  workbox.navigationPreload.enable();
-
-}
-
-
-
-self.addEventListener('fetch', (event) => {
-
-  if (event.request.mode === 'navigate') {
-
-    event.respondWith((async () => {
-
-      try {
-
-        const preloadResp = await event.preloadResponse;
-
-
-
-        if (preloadResp) {
-
-          return preloadResp;
-
-        }
-
-
-
-        const networkResp = await fetch(event.request);
-
-        return networkResp;
-
-      } catch (error) {
-
-
-
-        const cache = await caches.open(CACHE);
-
-        const cachedResp = await cache.match(offlineFallbackPage);
-
-        return cachedResp;
-
-      }
-
-    })());
-
-  }
-
-});
-
-
+);
